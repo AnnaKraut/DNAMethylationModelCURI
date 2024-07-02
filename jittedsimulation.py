bb6a9de8b43fe3a4241524d72b4afdc0235f15c7
joemandell <jg.mandell24@gmail.com>
2024-07-02 17:32:43 -0500

    Readability edits, added normal stats, added empirical mean stats

--- jittedsimulation.py
+++ jittedsimulation.py
@@ -3,15 +3,16 @@ import jittedswitch
 import matplotlib.pyplot as plt
 import scipy.stats as stats
 import numba
+import statistics
 from numba import prange
 
 
 #-----------parameterization-----------
 #TODO: add easier ways for users to input data
 #user should enter begin, end, step for the parameter they want to change.
-param_begin_val = 0.5
-param_end_val = 20
-step_count = 150
+param_begin_val = 0
+param_end_val = 3
+step_count = 50
 # define a parameter to vary - must be in the parameters dictionary - this should probably be selectable on command line
 param_to_change = "birth_rate"
 #define batch size - how many different runs should we average for each step? 
@@ -20,10 +21,10 @@ batch_size = 5000
 trial_max_length = 10000
 #define starting population
 totalpop = 100
-methylatedpop = 90
-unmethylatedpop = 10
+methylatedpop = 10
+unmethylatedpop = 90
 #SwitchDirection - a simulation terminates when it reaches this state
-SwitchDirection = -1 #1 -> mostly methylated, -1-> mostly unmethylated
+SwitchDirection = 1 #1 -> mostly methylated, -1-> mostly unmethylated
 #-----------Rates Dictionary---------
 default_parameters = {"r_hm": 0.5,          #0
                       "r_hm_m": 20/totalpop, #1
@@ -68,7 +69,7 @@ index_to_change = default_indices[param_to_change]
 
 #find the size of each step, rounded to 5 decimal places.
 step_size = round((param_end_val-param_begin_val)/step_count, 5)
-steps_to_test = step_count
+# step_array = step_count
 
 #-----------simulation-----------
 @numba.jit(nopython=True, parallel=True)
@@ -89,7 +90,7 @@ def main(rngs):
         # plt.close() #ensure the previous graph is done
         # plt.hist(valid_array, bins=200)
         # #generate strings, we will concatenate these into a single title string for the graphs
-        # param_string = "parameter: " + str(param_to_change) +  " = " + str(steps_to_test[step]) + " -> Exponential Parameter = " + str(exponential_parameters[step])
+        # param_string = "parameter: " + str(param_to_change) +  " = " + str(step_array[step]) + " -> Exponential Parameter = " + str(exponential_parameters[step])
         # step_string = "Step " + str(step+1) + "/" + str(step_count)
         # batch_string = "Batch of " + str(batch_size) + ", running for " + str(trial_max_length) + " steps each " + str(batch_size-valid_size) + " failed to finish"
         # plt.title(param_string + "\n" + step_string + "\n" + batch_string)
@@ -99,16 +100,24 @@ def main(rngs):
     
 #-----------setup-----------
 
-#generate the arrays for our output - default is negative to show these are undesirable results - maybe change to none instead
+#generate the arrays for our output - None (or null value) is the default
 exponential_parameters = [None] * step_count
+exponential_KS = [None] * step_count
+
 gamma_shape = [None] * step_count
 gamma_location = [None] * step_count
 gamma_scale = [None] * step_count
-exponential_KS = [None] * step_count
 gamma_KS = [None] * step_count
+
+normal_KS = [None] * step_count
+normal_sd = [None] * step_count
+normal_mean = [None] * step_count
+
 timeouts = [0] * step_count
+empirical_mean = [None] * step_count
+
 #list comprehension that creates an array of the values we tested for our chosen parameter
-steps_to_test = [step_size * i for i in range(step_count)]
+step_array = [step_size * i for i in range(step_count)]
 
 #create an array of random number generators that we will pass into our function
 #this makes it easier to reproduce, and also keeps Numba happy.
@@ -134,32 +143,45 @@ for step in range(step_count):
     if len(valid_array) > batch_size/2:
         #fit distributions to the data
         exponential_parameters[step] = stats.expon.fit(valid_array,floc=0)[1]
-        gamma_shape[step],gamma_location[step],gamma_scale[step]=stats.gamma.fit(valid_array)
+
+        # gamma_shape[step],gamma_location[step],gamma_scale[step]=stats.gamma.fit(valid_array)
+
+        normal_mean[step], normal_sd[step] =  stats.norm.fit(valid_array,)
+        print(f'Normal mean is {normal_mean[step]} and S.D. is {normal_sd[step]}')
+
+        empirical_mean[step] = statistics.fmean(valid_array)
 
         #calculate error for parameters with Kolmogorov-Smirnov test
-        #TODO: add args for expon
-        exponential_KS[step] = 10 * (stats.kstest(valid_array, stats.expon.cdf, N=len(valid_array)).statistic)
+        #note that we lock the first argument, location, to 0 for the exponential distribution
+        exponential_KS[step] = 10 * (stats.kstest(valid_array, 'expon', N=len(valid_array), args=(0,exponential_parameters[step])).statistic)
         print(exponential_KS[step])
-        #TODO: 
-        gamma_KS[step] = 10 * (stats.kstest(valid_array, stats.gamma.cdf, N=len(valid_array), args=(gamma_shape[step],0,gamma_scale[step])).statistic)
-        print(gamma_KS[step])
+        normal_KS[step] = 10 * (stats.kstest(valid_array, 'norm', N=len(valid_array), args=(normal_mean[step], normal_sd[step])).statistic)
+        print(exponential_KS[step])
+        # gamma_KS[step] = 10 * (stats.kstest(valid_array, stats.gamma.cdf, N=len(valid_array), args=(gamma_shape[step],0,gamma_scale[step])).statistic)
+        # print(gamma_KS[step])
 
     # print("predicted exponential parameter: ", exponential_parameters[step])
-    print("predicted gamma shape parameter: ", gamma_shape[step])
-    print("timed-out simulations: " + str(timeouts[step]) + " out of " + str(batch_size))
+    # print("predicted gamma shape parameter: ", gamma_shape[step])
+    print("timed-out simulations: " + str(raw_timeouts) + " out of " + str(batch_size))
 
 #-----------graphing-----------
 
 plt.close()
-final_label = "Switching times from methylated to unmethylated as birth rate changes \n Population = 100"
+final_label = "Switching times from unmethylated to methylated as birth rate changes \n Population = 100"
 run_stats = "Batches of " + str(batch_size) + ", running for maximum of " + str(trial_max_length) + " steps each"
-plt.plot(steps_to_test, exponential_parameters,label="exponential parameters")
-plt.plot(steps_to_test,timeouts, label = "proportion timed out, scaled by 10x")
-plt.plot(steps_to_test, exponential_KS, label="Exponential KS error, scaled by 10x")
-plt.plot(steps_to_test, gamma_shape,label="Gamma shape")
-plt.plot(steps_to_test, gamma_location,label="Gamma location")
-plt.plot(steps_to_test, gamma_scale,label="Gamma scale")
-plt.plot(steps_to_test, gamma_KS, label="Gamma KS error, scaled by 10x")
+plt.plot(step_array, exponential_parameters,label="exponential parameters", linestyle='dashed')
+plt.plot(step_array,timeouts, label = "proportion timed out, scaled by 10x")
+plt.plot(step_array, exponential_KS, label="Exponential KS error, scaled by 10x")
+
+# plt.plot(step_array, gamma_shape,label="Gamma shape")
+# plt.plot(step_array, gamma_location,label="Gamma location")
+# plt.plot(step_array, gamma_scale,label="Gamma scale")
+# plt.plot(step_array, gamma_KS, label="Gamma KS error, scaled by 10x")
+
+plt.plot(step_array, normal_mean, label='Normal mean',marker='.',linestyle='')
+plt.plot(step_array, normal_sd, label='Normal S.D.',marker='.',linestyle='')
+plt.plot(step_array, normal_KS, label="Normal KS error, scaled by 10x",marker='.',linestyle='')
+# plt.plot(step_array, empirical_mean, label='Empirical Mean', linestyle='dashed')
 plt.title(final_label + "\n" + run_stats)
 plt.xlabel('Value of parameter '+ param_to_change)
 plt.ylabel('Exponential parameter of switching time distribution')
